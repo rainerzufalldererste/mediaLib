06a5d0ab1a99f51bddd6b65f8d5265ca0cdc21c4
Christoph Stiller <cstiller@euclideon.com>
2018-08-13 15:00:30 +1000

    adding glew

--- TestApp/src/main.cpp
+++ TestApp/src/main.cpp
@@ -4,17 +4,19 @@
 #include "mThreadPool.h"
 #include "SDL.h"
 #include <time.h>
+#include "GL/glew.h"
+#include "GL/wglew.h"
 
 mVec2s resolution;
 SDL_Window *pWindow = nullptr;
-uint32_t *pPixels = nullptr;
+SDL_Surface *pSurface = nullptr;
 const size_t subScale = 5;
 mPtr<mImageBuffer> bgraImageBuffer = nullptr;
 mPtr<mThreadPool> threadPool = nullptr;
-mPtr<mMediaFileWriter> mediaFileWriter = nullptr;
+bool is3dEnabled = false;
 
-mFUNCTION(ResizeWindow, const mVec2s &newSize);
 mFUNCTION(OnVideoFramCallback, mPtr<mImageBuffer> &, const mVideoStreamType &videoStreamType);
+mFUNCTION(RenderFrame);
 
 int main(int, char **)
 {
@@ -28,52 +30,36 @@ int main(int, char **)
   mDEFER_DESTRUCTION(&mediaFileHandler, mMediaFileInputHandler_Destroy);
   mERROR_CHECK(mMediaFileInputHandler_Create(&mediaFileHandler, nullptr, L"C:/Users/cstiller/Videos/Converted.mp4", mMediaFileInputHandler_CreateFlags::mMMFIH_CF_VideoEnabled));
 
-  mERROR_CHECK(mMediaFileInputHandler_GetVideoStreamResolution(mediaFileHandler, &resolution));
+  SDL_Init(SDL_INIT_EVERYTHING);
 
-  mMediaFileInformation fileInfo = { 0 };
-  fileInfo.averageBitrate = 50 * 1024 * 1024;
-  fileInfo.frameRateFraction = mVec2s(30, 1);
-  fileInfo.frameSize = resolution;
-  fileInfo.pixelAspectRatioFraction = mVec2s(1, 1);
+  SDL_DisplayMode displayMode;
+  SDL_GetCurrentDisplayMode(0, &displayMode);
 
-  mDEFER_DESTRUCTION(&mediaFileWriter, mMediaFileWriter_Destroy);
-  mERROR_CHECK(mMediaFileWriter_Create(&mediaFileWriter, nullptr, L"C:/Users/cstiller/Videos/Export.mp4", &fileInfo));
+  resolution.x = displayMode.w;
+  resolution.y = displayMode.h;
 
   mDEFER_DESTRUCTION(pWindow, SDL_DestroyWindow);
-  pWindow = SDL_CreateWindow("VideoStream Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, (int)resolution.x / subScale, (int)resolution.y / subScale, 0);
+  pWindow = SDL_CreateWindow("VideoStream Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, (int)resolution.x, (int)resolution.y, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL);
   mERROR_IF(pWindow == nullptr, mR_ArgumentNull);
-  pPixels = (uint32_t *)SDL_GetWindowSurface(pWindow)->pixels;
-  mERROR_IF(pPixels == nullptr, mR_ArgumentNull);
+
+  if (SDL_GL_SetAttribute(SDL_GL_STEREO, 1) == 0)
+  {
+    is3dEnabled = true;
+    mPRINT("3d enabled.");
+  }
+
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+
+  pSurface = SDL_GetWindowSurface(pWindow);
+  mERROR_IF(pSurface == nullptr, mR_ArgumentNull);
 
   mDEFER_DESTRUCTION(&bgraImageBuffer, mImageBuffer_Destroy);
   mERROR_CHECK(mImageBuffer_Create(&bgraImageBuffer, nullptr, resolution));
 
-  mPtr<mImageBuffer> image;
-  mDEFER_DESTRUCTION(&image, mImageBuffer_Destroy);
-  mERROR_CHECK(mImageBuffer_CreateFromFile(&image, nullptr, "C:/Users/cstiller/Pictures/avatar.png"));
-  mERROR_CHECK(mImageBuffer_SaveAsJpeg(image, "C:/Users/cstiller/Pictures/avatar.jpg"));
-  mERROR_CHECK(mImageBuffer_AllocateBuffer(bgraImageBuffer, image->currentSize));
-  mERROR_CHECK(mPixelFormat_TransformBuffer(image, bgraImageBuffer));
-  mERROR_CHECK(ResizeWindow(image->currentSize));
-  mERROR_CHECK(mMemcpy(pPixels, (uint32_t *)bgraImageBuffer->pPixels, bgraImageBuffer->currentSize.x * bgraImageBuffer->currentSize.y));
-
-  SDL_UpdateWindowSurface(pWindow);
-
   mERROR_CHECK(mMediaFileInputHandler_SetVideoCallback(mediaFileHandler, OnVideoFramCallback));
   mERROR_CHECK(mMediaFileInputHandler_Play(mediaFileHandler));
-
-  mERROR_CHECK(mMediaFileWriter_Finalize(mediaFileWriter));
-
-  mRETURN_SUCCESS();
-}
-
-mFUNCTION(ResizeWindow, const mVec2s &newSize)
-{
-  mFUNCTION_SETUP();
-
-  SDL_SetWindowSize(pWindow, (int)newSize.x, (int)newSize.y);
-  pPixels = (uint32_t *)SDL_GetWindowSurface(pWindow)->pixels;
-  mERROR_IF(pPixels == nullptr, mR_ArgumentNull);
 
   mRETURN_SUCCESS();
 }
@@ -82,30 +68,34 @@ mFUNCTION(OnVideoFramCallback, mPtr<mImageBuffer> &buffer, const mVideoStreamTyp
 {
   mFUNCTION_SETUP();
 
-  const clock_t now = clock();
+  mUnused(videoStreamType);
 
   if (buffer->currentSize != bgraImageBuffer->currentSize)
-  {
-    ResizeWindow(buffer->currentSize / subScale);
     mERROR_CHECK(mImageBuffer_AllocateBuffer(bgraImageBuffer, buffer->currentSize, bgraImageBuffer->pixelFormat));
-  }
 
   mERROR_CHECK(mPixelFormat_TransformBuffer(buffer, bgraImageBuffer, threadPool));
-  mPRINT("frame time: %" PRIi32 "\n", clock() - now);
-
-  mERROR_CHECK(mMediaFileWriter_AppendVideoFrame(mediaFileWriter, bgraImageBuffer));
-
-  size_t i = 0;
-
-  for (size_t y = 0; y < videoStreamType.resolution.y; y += subScale)
-    for (size_t x = 0; x < videoStreamType.resolution.x; x += subScale)
-      pPixels[i++] = ((uint32_t *)(bgraImageBuffer->pPixels))[x + y * videoStreamType.resolution.x];
-
-  SDL_UpdateWindowSurface(pWindow);
 
   SDL_Event sdl_event;
   while (SDL_PollEvent(&sdl_event))
     ; // We don't care.
+
+  mERROR_CHECK(RenderFrame());
+
+  mRETURN_SUCCESS();
+}
+
+size_t frame = 0;
+
+mFUNCTION(RenderFrame)
+{
+  mFUNCTION_SETUP();
+
+  glClearColor(frame / 255.0f, 0.0f, 0.0f, 1.0f);
+  glClear(GL_DEPTH_BUFFER_BIT);
+
+
+
+  SDL_UpdateWindowSurface(pWindow);
 
   mRETURN_SUCCESS();
 }
