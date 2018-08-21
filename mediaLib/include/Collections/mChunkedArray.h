85fcc9cfe74503cf100950da6797af073591258c
Christoph Stiller <cstiller@euclideon.com>
2018-08-21 19:56:29 +1000

    stuff wip

---
+++ mediaLib/include/Collections/mChunkedArray.h
@@ -0,0 +1,47 @@
+// Copyright 2018 Christoph Stiller
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions :
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#ifndef mChunkedArray_h__
+#define mChunkedArray_h__
+
+#include "default.h"
+
+template <typename T>
+struct mChunkedArray
+{
+  struct mChunkedArrayBlock
+  {
+    T *pData;
+    size_t blockSize;
+    size_t blockStartIndex;
+    size_t blockEndIndex;
+  };
+
+  size_t blockSize;
+  size_t blockCount;
+  size_t itemCount;
+  mChunkedArrayBlock *pBlocks;
+  mAllocator *pAllocator;
+};
+
+template <typename T>
+mFUNCTION(mChunkedArray_Create(OUT mPtr<mChunkedArray<T>> *pChunkedArray, IN mAllocator *pAllocator, const size_t blockSize = 64));
+
+template <typename T>
+mFUNCTION(mChunkedArray_Destroy(IN_OUT mPtr<mChunkedArray<T>> *pChunkedArray));
+
+template <typename T>
+mFUNCTION(mChunkedArray_Push(mPtr<mChunkedArray<T>> &chunkedArray, IN T *pItem, const OUT OPTIONAL size_t *pIndex));
+
+template <typename T>
+mFUNCTION(mChunkedArray_PushBack(mPtr<mChunkedArray<T>> &chunkedArray, const size_t index, IN T *pItem));
+
+template <typename T>
+mFUNCTION(mChunkedArray_GetCount(mPtr<mChunkedArray<T>> &chunkedArray, OUT const size_t *pIndex));
+
+#endif // mChunkedArray_h__
