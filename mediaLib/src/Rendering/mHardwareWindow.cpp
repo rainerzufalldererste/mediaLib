169a82640cafee9b4026afa4a8e34dcbf7d58916
Christoph Stiller <cstiller@euclideon.com>
2018-08-28 17:05:13 +1000

    optional stereo 3d

--- mediaLib/src/Rendering/mHardwareWindow.cpp
+++ mediaLib/src/Rendering/mHardwareWindow.cpp
@@ -12,6 +12,7 @@ struct mHardwareWindow
 {
   SDL_Window *pWindow;
   mRenderContextId renderContextID;
+  bool no3dAvailableFlag;
 };
 
 mFUNCTION(mHardwareWindow_Create_Internal, IN mHardwareWindow *pWindow, const std::string & title, const mVec2s & size, const mHardwareWindow_DisplayMode displaymode);
@@ -29,7 +30,13 @@ mFUNCTION(mHardwareWindow_Create, OUT mPtr<mHardwareWindow>* pWindow, IN mAlloca
 
   mERROR_CHECK(mRenderParams_CreateRenderContext(&(*pWindow)->renderContextID, *pWindow));
 
+  mGL_ERROR_CHECK();
+
   mERROR_CHECK(mHardwareWindow_SetAsActiveRenderTarget(*pWindow));
+
+  mPRINT("GPU: %s %s\nDriver Version: %s\nGLSL Version: %s\n", glGetString(GL_VENDOR), glGetString(GL_RENDERER), glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+  mGL_ERROR_CHECK();
 
   mRETURN_SUCCESS();
 }
@@ -121,12 +128,23 @@ mFUNCTION(mHardwareWindow_Create_Internal, IN mHardwareWindow *pWindow, const st
 
   mERROR_IF(size.x > INT_MAX || size.y > INT_MAX, mR_ArgumentOutOfBounds);
 
+  bool try3d = true;
+retry_without_3d:
+  SDL_GL_SetAttribute(SDL_GL_STEREO, try3d ? 1 : 0);
+
   pWindow->pWindow = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, (int)size.x, (int)size.y
     , 
 #if defined(mRENDERER_OPENGL)
     SDL_WINDOW_OPENGL |
 #endif
     displaymode);
+
+  if (pWindow->pWindow == nullptr && try3d)
+  {
+    try3d = false;
+    goto retry_without_3d;
+  }
+
   mERROR_IF(pWindow->pWindow == nullptr, mR_InternalError);
 
   mRETURN_SUCCESS();
