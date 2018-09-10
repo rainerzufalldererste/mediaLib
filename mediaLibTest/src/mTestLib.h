070f278931c433aa560f9cdf7e140d50cff207bf
Christoph Stiller <cstiller@euclideon.com>
2018-09-10 20:12:21 +1000

    Adding queue unit tests

--- mediaLibTest/src/mTestLib.h
+++ mediaLibTest/src/mTestLib.h
@@ -12,7 +12,7 @@
 #include "default.h"
 #include "gtest/gtest.h"
 
-//#define mDEBUG_TESTS
+#define mDEBUG_TESTS
 
 #ifdef mDEBUG_TESTS
 #define mTEST_FAIL() do { printf("Test Failed at " __FUNCTION__ " in File '" __FILE__ "' Line %" PRIi32 ".", __LINE__); __debugbreak(); } while(0)
