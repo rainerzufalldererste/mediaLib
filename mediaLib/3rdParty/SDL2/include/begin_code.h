afb403b0807e3d2c431fe0e6f3dbd46a3d3d2df6
Christoph Stiller <c.stiller@live.de>
2021-08-05 10:28:08 +0200

    Updating SDL2

--- mediaLib/3rdParty/SDL2/include/begin_code.h
+++ mediaLib/3rdParty/SDL2/include/begin_code.h
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 1997-2019 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2020 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -51,33 +51,26 @@
 
 /* Some compilers use a special export keyword */
 #ifndef DECLSPEC
-#define DECLSPEC
+# if defined(__WIN32__) || defined(__WINRT__) || defined(__CYGWIN__)
+#  ifdef DLL_EXPORT
+#   define DECLSPEC __declspec(dllexport)
+#  else
+#   define DECLSPEC
+#  endif
+# elif defined(__OS2__)
+#   ifdef BUILD_SDL
+#    define DECLSPEC    __declspec(dllexport)
+#   else
+#    define DECLSPEC
+#   endif
+# else
+#  if defined(__GNUC__) && __GNUC__ >= 4
+#   define DECLSPEC __attribute__ ((visibility("default")))
+#  else
+#   define DECLSPEC
+#  endif
+# endif
 #endif
-//#ifndef DECLSPEC
-//# if defined(__WIN32__) || defined(__WINRT__)
-//#  ifdef __BORLANDC__
-//#   ifdef BUILD_SDL
-//#    define DECLSPEC
-//#   else
-//#    define DECLSPEC    __declspec(dllimport)
-//#   endif
-//#  else
-//#   define DECLSPEC __declspec(dllexport)
-//#  endif
-//# elif defined(__OS2__)
-//#   ifdef BUILD_SDL
-//#    define DECLSPEC    __declspec(dllexport)
-//#   else
-//#    define DECLSPEC
-//#   endif
-//# else
-//#  if defined(__GNUC__) && __GNUC__ >= 4
-//#   define DECLSPEC __attribute__ ((visibility("default")))
-//#  else
-//#   define DECLSPEC
-//#  endif
-//# endif
-//#endif
 
 /* By default SDL uses the C calling convention */
 #ifndef SDLCALL
